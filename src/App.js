aec83395f0b96a7c9741e376c0bedbfd123ad97a
ibrah00 <ibrahem.abdul@lc.cuny.edu>
2020-09-25 20:22:54 -0400

    modified App.js to say Ibrahem

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello and Welcome
+        My name is Ibrahem and I'm going to master React, hopefully!
         </p>
         <a
           className="App-link"
